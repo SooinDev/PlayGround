11782787cd9d2190e87e4ad20f95a6b717f6c04c
SooinDev <alwayswithsound@gmail.com>
2025-09-15 13:26:29 +0900

    비밀번호 암호화(회원가입)

--- src/main/java/com/playground/service/impl/MemberServiceImpl.java
+++ src/main/java/com/playground/service/impl/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import com.playground.mapper.MemberMapper;
 import com.playground.service.MemberService;
 import com.playground.vo.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,22 +15,28 @@ public class MemberServiceImpl implements MemberService {
   @Autowired
   MemberMapper memberMapper;
 
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
   @Override
   public void register(MemberVO memberVO) throws EmailDuplicateException, NicknameDuplicateException {
 
     // 1. 이메일 중복 체크
     if (memberMapper.isEmailDuplicated(memberVO.getEmail())) {
-      // "이메일 중복"이라는 명확한 원인을 담아 예외를 발생시킴
       throw new EmailDuplicateException("이미 사용 중인 이메일입니다.");
     }
 
     // 2. 닉네임 중복 체크
     if (memberMapper.isNicknameDuplicated(memberVO.getNickname())) {
-      // "닉네임 중복"이라는 명확한 원인을 담아 예외를 발생시킴
       throw new NicknameDuplicateException("이미 사용 중인 닉네임입니다.");
     }
 
-    // 3. 모든 검사를 통과하면 DB에 저장
+    // 3. 비밀번호 암호화
+    String rawPassword = memberVO.getPassword();
+    String encodedPassword = passwordEncoder.encode(rawPassword);
+    memberVO.setPassword(encodedPassword);
+
+    // 4. DB에 저장
     memberMapper.insertMember(memberVO);
   }
 }
